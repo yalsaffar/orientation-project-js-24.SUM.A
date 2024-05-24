02126d290ec5a3fb14bb8ddb654e211c77aa1a99
esohel30 <eric.sohel@stonybrook.edu>
2024-05-24 17:29:50 -0400

    started working on the experience form; will add the css file within next commit

--- src/ExperienceForm.js
+++ src/ExperienceForm.js
@@ -1,1 +1,15 @@
 import React, { useState } from 'react';
+
+function ExperienceForm() {
+  const [formData, setFormData] = useState({
+    title: '',
+    company: '',
+    start_date: '',
+    end_date: '',
+    description: '',
+    logo: ''
+  });
+
+}
+
+export default ExperienceForm;
