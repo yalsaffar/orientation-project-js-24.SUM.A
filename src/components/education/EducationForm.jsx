10c397b116e229cf2185b9f913360b209a289c20
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2024-05-30 00:09:03 -0700

    Added alert for successful or unsuccesful submission

--- src/components/education/EducationForm.jsx
+++ src/components/education/EducationForm.jsx
@@ -29,7 +29,7 @@ const EducationForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        fetch('resume/education', {
+        fetch('http://localhost:5000/resume/education', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -38,13 +38,11 @@ const EducationForm = () => {
         })
         .then(response => response.json())
         .then(data => {
-            console.log('Success:', data)
+            alert("Education successfully added!")
         })
         .catch((error) => {
-            console.log("Error:", error)
+            alert("Error: Education not added :(")
         })
-        //TO-DO: Add error handling for incorrect values (e.g. grade > 100)
-        //TO-DO: Add submission success modal
     }
 
     return (
