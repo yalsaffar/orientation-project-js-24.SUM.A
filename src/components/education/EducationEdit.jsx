487319890b957c07e16b769606b8c821ba9733ab
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2024-05-30 00:43:49 -0700

    Add education edit component and update styling for educagtion view

---
+++ src/components/education/EducationEdit.jsx
@@ -0,0 +1,17 @@
+const EducationEdit = (education) => {
+    // const [formData, setFormData] = useState ({
+    //     course: '',
+    //     school: '',
+    //     start_date: '',
+    //     end_date: '',
+    //     grade: '',
+    //     logo: ''
+    // })
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default EducationEdit;
--- src/components/education/EducationView.jsx
+++ src/components/education/EducationView.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
-import './EducationView.css'
+import EducationEdit from "./EducationEdit";
 import defaultLogo from '../../assets/graduation-cap.png'
 import editIcon from '../../assets/edit.png'
+import './EducationView.css'
 
 const EducationView = () => {
     const [educationData, setEducationData] = useState([]);
 
     useEffect(() => {
-        fetch("/resume/education")
+        fetch("http://localhost:5000/resume/education")
             .then((res) => {
                 res.json().then((data) => {
                 setEducationData(data);
@@ -20,18 +21,18 @@ const EducationView = () => {
         {educationData ?
             educationData.map(education =>
                 <div key={education.id} className="education-item">
-                    <div>
-                        <img src={editIcon} alt="editIcon" className="icon"/>
-                        <div className="education-item-school">
-                            {education.logo.includes("example") ? <img src={defaultLogo} alt="defaultLogo" className="logo"/> : <img src={education.logo} alt="logo" className="logo"/>}
-                            <h3>{education.school}</h3>
+                        <div className="education-item-school-container">
+                            <div className="education-item-school">
+                                {education.logo.includes("example") ? <img src={defaultLogo} alt="defaultLogo" className="logo"/> : <img src={education.logo} alt="logo" className="logo"/>}
+                                <h3>{education.school}</h3>
+                            </div>
+                            <button className="icon-button"><img src={editIcon} alt="editIcon" className="icon"/></button>
                         </div>
                         <div className="education-item-course">
                             <li>{education.course}</li>
                             <li>Grade: {education.grade}</li>
                             <li>{education.start_date} - {education.end_date}</li>
                         </div>
-                    </div>
                 </div>
                 )
             : ""}
