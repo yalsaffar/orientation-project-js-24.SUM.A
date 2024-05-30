80656a0840cd9ba38dffa599202307d5b02a8ecd
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2024-05-30 00:27:53 -0700

    Add edit icon

--- src/components/education/EducationView.jsx
+++ src/components/education/EducationView.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import './EducationView.css'
 import defaultLogo from '../../assets/graduation-cap.png'
+import editIcon from '../../assets/edit.png'
 
 const EducationView = () => {
     const [educationData, setEducationData] = useState([]);
@@ -19,14 +20,17 @@ const EducationView = () => {
         {educationData ?
             educationData.map(education =>
                 <div key={education.id} className="education-item">
-                    <div className="education-item-school">
-                        {education.logo.includes("example") ? <img src={defaultLogo} alt="defaultLogo" className="logo"/> : <img src={education.logo} alt="logo" className="logo"/>}
-                        <h3>{education.school}</h3>
-                    </div>
-                    <div className="education-item-course">
-                        <li>{education.course}</li>
-                        <li>Grade: {education.grade}</li>
-                        <li>{education.start_date} - {education.end_date}</li>
+                    <div>
+                        <img src={editIcon} alt="editIcon" className="icon"/>
+                        <div className="education-item-school">
+                            {education.logo.includes("example") ? <img src={defaultLogo} alt="defaultLogo" className="logo"/> : <img src={education.logo} alt="logo" className="logo"/>}
+                            <h3>{education.school}</h3>
+                        </div>
+                        <div className="education-item-course">
+                            <li>{education.course}</li>
+                            <li>Grade: {education.grade}</li>
+                            <li>{education.start_date} - {education.end_date}</li>
+                        </div>
                     </div>
                 </div>
                 )
