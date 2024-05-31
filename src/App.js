4fd6d493c37d503ee3cf05f379abb88b41c29494
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2024-05-31 08:51:32 -0700

    Fix toggleExperienceForm

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   };
 
   const toggleExperienceForm = () => {
-    setShowEducationForm(!showEducationForm);
+    setShowExperienceForm(!showEducationForm);
   }
 
   const toggleEducationForm = () => {
